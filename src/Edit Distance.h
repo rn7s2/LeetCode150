ef07ac5815a8c6a1d51b5c3a42027086548a4bef
rn7s2 <mail@rn7s2.cn>
2024-04-23 14:04:31 +0800

    one problem.

---
+++ src/Edit Distance.h
@@ -0,0 +1,35 @@
+#include "common.h"
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = j;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min({
+                        dp[i - 1][j - 1], // 替换 word1[i - 2] 为 word2[j - 2]
+                        dp[i - 1][j],     // 删除 word1[i - 2]
+                        dp[i][j - 1]      // 将 word2[j - 2] 插入 word1[i - 1]
+                    }) + 1;
+                }
+            }
+        }
+        
+        return dp[n][m];
+    }
+    
+    void run() {
+        cout << minDistance("horse", "ros") << endl;
+        cout << minDistance("intention", "execution") << endl;
+    }
+};
